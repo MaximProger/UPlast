a5f4d0edd6b5b92075c859e195bef111f4d0c68d
Maxim Koshman <maksimkoshman090@gmail.com>
2019-12-07 18:26:02 +0300

    first realize main page

---
+++ js/_custom.js
@@ -0,0 +1,9 @@
+$(function() {
+    // $('.table__edit').tooltip();
+
+    $('#pagList').on('click', function() {
+        $('#pgListValue').toggleClass('active');
+    })
+
+    $('.xdsoft_today_button').css('display', 'none');
+});
---
+++ js/libs__components.js
@@ -0,0 +1,30 @@
+
+// Hnts
+jQuery('.table__edit').tooltip();
+jQuery('.table__copy').tooltip();
+jQuery('.table__del').tooltip();
+
+// Calendar
+// jQuery('#datetimepicker-3').datetimepicker({
+//     timepicker:false,
+// });
+
+jQuery.datetimepicker.setLocale('ru');
+
+jQuery('#datetimepicker-3').datetimepicker({
+ i18n:{
+  de:{
+   months:[
+    'Januar','Februar','März','April',
+    'Mai','Juni','Juli','August',
+    'September','Oktober','November','Dezember',
+   ],
+   dayOfWeek:[
+    "So.", "Mo", "Di", "Mi", 
+    "Do", "Fr", "Sa.",
+   ]
+  }
+ },
+ timepicker:false,
+ format:'d.m.Y'
+});
