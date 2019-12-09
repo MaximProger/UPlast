7b872e092eaa08e6ea8f40c11dae7a230a550044
Maxim Koshman <maksimkoshman090@gmail.com>
2019-12-09 13:24:21 +0300

    full version site

--- js/_custom.js
+++ js/_custom.js
@@ -1,7 +1,5 @@
 $(function() {
-    // $('.table__edit').tooltip();
-
-
+    
     // Pagination List
     $('#pagList').on('click', function() {
         $('#pgListValue').toggleClass('active');
@@ -13,70 +11,47 @@ $(function() {
 
     // FirstItem
     $('#tableFirstItemShow').on('click', function() {
-        $('.table__line--firstItem').show(300);
-        $('#tableFirstItemShow').hide();
-        $('#tableFirstItemHide').show();
-
-        
-    })
-
-    $('#tableFirstItemHide').on('click', function() {
-        $('.table__line--firstItem').hide(300);
-        $('#tableFirstItemShow').show();
-        $('#tableFirstItemHide').hide();
-
+        $('.table__line--firstItem').toggleClass('show');
+        $('#tableFirstItemShow .fa-arrow-down').toggleClass('fa-arrow-up');
     })
 
     // SecondItem
     $('#tableSecondItemShow').on('click', function() {
-        $('.table__line--secondItem').show(300);
-        $('#tableSecondItemShow').hide();
-        $('#tableSecondItemHide').show();
-
-        
+        $('.table__line--secondItem').toggleClass('show');
+        $('#tableSecondItemShow .fa-arrow-down').toggleClass('fa-arrow-up');
     })
 
-    $('#tableSecondItemHide').on('click', function() {
-        $('.table__line--secondItem').hide(300);
-        $('#tableSecondItemShow').show();
-        $('#tableSecondItemHide').hide();
-
+     // ThirdItem
+     $('#tableThirdItemShow').on('click', function() {
+        $('.table__line--thirdItem').toggleClass('show');
+        $('#tableThirdItemShow .fa-arrow-down').toggleClass('fa-arrow-up');
     })
 
-    // ThirdItem
-    $('#tableThirdItemShow').on('click', function() {
-        $('.table__line--thirdItem').show(300);
-        $('#tableThirdItemShow').hide();
-        $('#tableThirdItemHide').show();
-
-        
+     // FourthItem
+     $('#tableFourthItemShow').on('click', function() {
+        $('.table__line--fourthItem').toggleClass('show');
+        $('#tableFourthItemShow .fa-arrow-down').toggleClass('fa-arrow-up');
     })
 
-    $('#tableThirdItemHide').on('click', function() {
-        $('.table__line--thirdItem').hide(300);
-        $('#tableThirdItemShow').show();
-        $('#tableThirdItemHide').hide();
+    
 
+    // Delite Function
+    $('.deliteBtn').click(function(){
+        $(this).parent().hide(100);
+       });
+
+    // Add new select
+
+    $('#addSelect').on('click', function() {
+        $('#formSelect').append(`<div class="select__wrapper">
+        <select class="form-control mt-2" id="exampleFormControlSelect4">
+                <option>Валиева Рената Ренатовна</option>
+                <option>Глухих Петр Иванович</option>
+                <option>Удочкин Александр Николаевич</option>
+            </select> <a href="#" class="select__del table__del deliteBtn" title="Удалить"><i
+                class="fas fa-trash-alt"></i></a>
+</div>`)
+        $('#addSelect').hide();
     })
-
-    // FourthItem
-    $('#tableFourthItemShow').on('click', function() {
-        $('.table__line--fourthItem').show(300);
-        $('#tableFourthItemShow').hide();
-        $('#tableFourthItemHide').show();
-
-        
-    })
-
-    $('#tableFourthItemHide').on('click', function() {
-        $('.table__line--fourthItem').hide(300);
-        $('#tableFourthItemShow').show();
-        $('#tableFourthItemHide').hide();
-
-    })
-
-
-
-
 
 });
